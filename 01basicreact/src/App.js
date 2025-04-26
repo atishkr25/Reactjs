c04a202cd58ebc4d300c361620c65be79d73e3a1
atishkumar <atishkumar6115@gmail.com>
2025-04-26 23:17:19 +0530

    second commit

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+   <h1>Let's goooo</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>Let's gooo with vite | Hii Atish this side</h1>
+  );
+}
+
+export default App;
